bceed93304ef75893ef8b0ea3f41fca73c677a93
guams <guams@duck.com>
2025-01-29 11:05:15 +0100

    Mesure de speedup sur pi.java

--- TP4/src/main/resources/plot.py
+++ TP4/src/main/resources/plot.py
@@ -1,30 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-NBR_COEURS = 8
+NBR_COEURS = 16
 
 def expected(x):
-    return x
+    return x  # La fonction attendue est linéaire
 
-file_path = 'output_assignment102.csv'
+file_path = 'output_pi_forte.csv'
 df = pd.read_csv(file_path, sep=";")
+t1 = df["Temps(ms)"][0]
 
 x = [nProc for nProc in df["Nprocessus"]]
-y = [(tp/nb_proc)/tp for nb_proc, tp in zip(df["Nprocessus"], df["Temps(ms)"])]
+y = [t1/tp for tp in df["Temps(ms)"]]
 
-x_expected = [i for i in range(len(x)+1)]
+x_expected = [i for i in range(NBR_COEURS + 2)]
 y_expected = [expected(j) for j in x_expected]
 
-plt.plot(x, y, label = f"Réalité (Expérience sur {format(df["Ntotal"][0], ".1E")} points)")
-plt.plot(x_expected, y_expected, '--', label = "Attendu")
+plt.figure(figsize=(10, 6))  # Optionnel : pour agrandir la figure
+plt.scatter(x, y, color='red', label=f"Réalité (Expérience sur {format(df['Ntotal'][0], '.1E')} points)")
+plt.plot(x_expected, y_expected, '--', label="Attendu")
 
 plt.xlim(1, NBR_COEURS)
+plt.ylim(0, 10)  # Ajustement de la limite y à 10
 
-plt.xticks(range(1, NBR_COEURS + 1, 1))
-plt.yticks(range(1, NBR_COEURS + 1, 1))
+# Échelles personnalisées
+plt.xticks(range(1, NBR_COEURS + 2, 1))  # Échelle de 1 en 1 pour les processus
+plt.yticks(range(0, 11))  # Échelle de 0 à 10 par pas de 1
+
+# Ajout d'une grille pour un repère orthonormé
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+plt.axhline(0, color='black', linewidth=0.5, ls='--')  # Ligne horizontale à y=0
+plt.axvline(0, color='black', linewidth=0.5, ls='--')  # Ligne verticale à x=0
 
 plt.xlabel('Nombre de processus')
 plt.ylabel('Speedup')
 plt.legend()
-plt.savefig('Speedup_MonteCarlo_machine_G26.png')
+plt.title('Graphique de Speedup en fonction du Nombre de Processus')  # Titre du graphique
+plt.savefig('Speedup_Scalabilite_forte_pi_machine_G26.png')
 plt.show()
